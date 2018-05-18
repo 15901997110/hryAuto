ed835368050519be7120775f321c957f8b4b67cc
luqiwei <luqiwei@kjt.com.cn>
2018-05-18 13:59:41 +0800

    Tserviceå¢插入数据时增加返回主键

---
+++ hryweb/src/main/java/com/haier/util/ImportUtil.java
@@ -0,0 +1,12 @@
+package com.haier.util;
+
+/**
+ * @Description: 导入服务和接口工具类
+ * @Author: luqiwei
+ * @Date: 2018/5/18 13:06
+ */
+public class ImportInterfaceUtil {
+    public static void importInterface(String url,String serviceKey,Boolean overwrite){
+
+    }
+}
