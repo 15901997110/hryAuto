4e64ad4fcbeab426aeba84fe4e8a6a41a4d7fab1
luqiwei <luqiwei@kjt.com.cn>
2018-05-22 16:59:03 +0800

    登录功能实现

--- hryweb/src/main/java/com/haier/controllers/UserController.java
+++ hryweb/src/main/java/com/haier/controllers/UserController.java
@@ -1,13 +1,20 @@
 package com.haier.controllers;
 
+import com.haier.enums.StatusCodeEnum;
 import com.haier.po.User;
+import com.haier.response.Result;
 import com.haier.service.UserService;
+import com.haier.util.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -15,20 +22,26 @@ import javax.servlet.http.HttpSession;
  * @Author: luqiwei
  * @Date: 2018/5/12 15:11
  */
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     UserService userService;
     @PostMapping("/login.do")
-    public String login(HttpSession session,String username,String password){
-        User user = userService.findUser(username, password);
+    public Result login(HttpSession session, String identity, String password){
+        User user = userService.findUser(identity, password);
         if(user==null){
-            return "login";
+            return ResultUtil.error(StatusCodeEnum.LOGIN_ERROR);
         }
         else{
-            session.setAttribute("loginusername",user.getIdentity());
-            return "index";
+            session.setAttribute("user",user.getIdentity());
+            return ResultUtil.success();
         }
+    }
+    @PostMapping("/logout")
+    public Result logout(HttpSession session){
+        //清除session
+        session.invalidate();
+        return ResultUtil.success();
     }
 }
