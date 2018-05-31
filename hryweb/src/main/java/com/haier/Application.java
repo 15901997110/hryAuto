25d333562b46566d3d4886adf80c75a00a9be317
luqiwei <luqiwei@kjt.com.cn>
2018-05-31 15:38:39 +0800

    打包配置更新

--- hryweb/src/main/java/com/haier/Application.java
+++ hryweb/src/main/java/com/haier/Application.java
@@ -3,11 +3,13 @@ package com.haier;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@SpringBootApplication//(exclude = {  DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 @MapperScan("com.haier.mapper")
 public class Application {
     public static void main(String[] args){
