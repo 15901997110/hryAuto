41352abb2ea71325013255d668af62b43cfe24e4
luqiwei <luqiwei@kjt.com.cn>
2018-06-01 18:42:59 +0800

    暂时注释登录拦截器

--- hryweb/src/main/java/com/haier/interceptor/LoginInterceptor.java
+++ hryweb/src/main/java/com/haier/interceptor/LoginInterceptor.java
@@ -20,8 +20,8 @@ import javax.servlet.http.HttpSession;
 public class LoginInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //return true;//暂时关闭拦截器功能便于调试
-        HttpSession session = request.getSession();
+        return true;//暂时关闭拦截器功能便于调试
+        /*HttpSession session = request.getSession();
         if(session.getAttribute("userSession")!=null){//服务端session信息存在
             log.debug("userSession:"+session.getAttribute("userSession").toString());
             Cookie[] cookies = request.getCookies();
@@ -48,7 +48,7 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 
         //跳转登录页面
         response.sendRedirect("/login.html");
-        return false;
+        return false;*/
     }
 
 }
