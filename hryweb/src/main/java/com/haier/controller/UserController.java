ed1588f77defd638f6ac0eb095b8eb06f106ac9d
luqiwei <luqiwei@kjt.com.cn>
2018-06-11 09:21:40 +0800

    登出做非空校验

--- hryweb/src/main/java/com/haier/controller/UserController.java
+++ hryweb/src/main/java/com/haier/controller/UserController.java
@@ -31,7 +31,7 @@ public class UserController {
 
     //按主键查询用户
     @PostMapping("/selectOne.do")
-    public Result selectOne(Integer id){
+    public Result selectOne(Integer id) {
         return ResultUtil.success(userService.selectOne(id));
     }
 
@@ -39,62 +39,61 @@ public class UserController {
 
     //新增用户
     @PostMapping("/insertOne.do")
-    public Result insertOne(User user){
+    public Result insertOne(User user) {
         return ResultUtil.success(userService.insertOne(user));
     }
 
     //更新用户
     @PostMapping("/updateOne.do")
-    public Result updateOne(User user){
-        return ResultUtil.success(userService.updateOne(user.getId(),user));
+    public Result updateOne(User user) {
+        return ResultUtil.success(userService.updateOne(user.getId(), user));
     }
 
     //删除用户
     @PostMapping("/deleteOne.do")
-    public Result deleteOne(Integer id){
+    public Result deleteOne(Integer id) {
         return ResultUtil.success(userService.deleteOne(id));
     }
 
     //综合查询
     @PostMapping("/selectByCondition.do")
-    public Result selectByCondition(User user,Integer pageNum,Integer pageSize){
-        return ResultUtil.success(userService.selectByCondition(user,pageNum,pageSize));
+    public Result selectByCondition(User user, Integer pageNum, Integer pageSize) {
+        return ResultUtil.success(userService.selectByCondition(user, pageNum, pageSize));
     }
 
     //根据GroupId查询相应组的用户列表
     @PostMapping("/selectByGroupId.do")
-    public Result selectByGroupId(Integer groupId){
+    public Result selectByGroupId(Integer groupId) {
         return ResultUtil.success(userService.selectByGroupId(groupId));
     }
 
     //修改用户密码
     @PostMapping("/modifyPwd.do")
-    public Result modifyPwd(String identity,String oldPwd,String newPwd){
-        return ResultUtil.success(userService.modifyPwd(identity,oldPwd,newPwd));
+    public Result modifyPwd(String identity, String oldPwd, String newPwd) {
+        return ResultUtil.success(userService.modifyPwd(identity, oldPwd, newPwd));
     }
 
     //登录
     @PostMapping("/login.do")
-    public Result login(HttpServletRequest request,HttpServletResponse response, String identity, String password){
+    public Result login(HttpServletRequest request, HttpServletResponse response, String identity, String password) {
         HttpSession session;
         User user = userService.findUser(identity, password);
-        if(user==null){
+        if (user == null) {
             return ResultUtil.error(StatusCodeEnum.LOGIN_ERROR);
-        }
-        else{
-            session=request.getSession(true);
-            session.setMaxInactiveInterval(60*60*24*30);//秒为单位,设置一个月的有效时间
-            session.setAttribute("userSession",user.getIdentity());
+        } else {
+            session = request.getSession(true);
+            session.setMaxInactiveInterval(60 * 60 * 24 * 30);//秒为单位,设置一个月的有效时间
+            session.setAttribute("userSession", user.getIdentity());
             //设置cookie信息
-            Cookie identityCookie=new Cookie("identityCookie",user.getIdentity());
+            Cookie identityCookie = new Cookie("identityCookie", user.getIdentity());
             identityCookie.setPath("/");
             identityCookie.setMaxAge(Integer.MAX_VALUE);//设置cookie永不过期.setMaxAge单位为秒
 
-            Cookie realnameCookie=new Cookie("realnameCookie",user.getRealname());
+            Cookie realnameCookie = new Cookie("realnameCookie", user.getRealname());
             realnameCookie.setPath("/");
             realnameCookie.setMaxAge(Integer.MAX_VALUE);
 
-            Cookie groupidCookie=new Cookie("groupidCookie",user.getGroupid().toString());
+            Cookie groupidCookie = new Cookie("groupidCookie", user.getGroupid().toString());
             groupidCookie.setPath("/");
             groupidCookie.setMaxAge(Integer.MAX_VALUE);
 
@@ -108,11 +107,17 @@ public class UserController {
 
     //登出
     @PostMapping("/logout.do")
-    public Result logout(HttpSession session){
-        log.debug("用户session为:"+session.getAttribute("userSession").toString());
-        //清除session即可
-        session.invalidate();
-        log.debug("用户session清除成功");
+    public Result logout(HttpSession session) {
+        if (session != null) {
+            Object userSession;
+            userSession = session.getAttribute("userSession");
+            if(userSession!=null){
+                log.debug("用户session为:" + userSession.toString());
+            }
+            //清除session即可
+            session.invalidate();
+            log.debug("用户session清除成功");
+        }
         return ResultUtil.success();
     }
 
