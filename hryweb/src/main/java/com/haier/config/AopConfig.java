62860ce8100145a0eeb3cd6ddf424076e3c39262
luqiwei <luqiwei@kjt.com.cn>
2018-06-12 17:19:57 +0800

    aop around

--- hryweb/src/main/java/com/haier/config/AopConfig.java
+++ hryweb/src/main/java/com/haier/config/AopConfig.java
@@ -1,21 +1,36 @@
 package com.haier.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 /**
  * @Description: 使用aop对发送请求的参数进行前置增加
  * @Author: luqiwei
  * @Date: 2018/6/12 14:11
  */
-@Configuration
-@Aspect
+@Slf4j
+@Component
+@Aspect//描述切面类
 public class AopConfig {
     //切入点
     @Pointcut("execution(* com.haier.util.HryHttpClientUtil.send(..))")
-    public void pointcut(){};
+    public void execute(){};
 
+    @Around("execute()")
+    public void doBefore(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object[] args = proceedingJoinPoint.getArgs();
+        for(Object o:args){
+            log.debug(o.toString());
+        }
+        Object proceed = proceedingJoinPoint.proceed();
+
+
+    }
 
 }
