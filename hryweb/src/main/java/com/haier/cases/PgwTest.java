a5af690f7df8e51c87cf4f0aa889e48b7ae732b4
luqiwei <luqiwei@kjt.com.cn>
2018-06-26 15:42:05 +0800

    pgwTest.java

--- hryweb/src/main/java/com/haier/cases/PgwTest.java
+++ hryweb/src/main/java/com/haier/cases/PgwTest.java
@@ -1,6 +1,11 @@
 package com.haier.cases;
 
 import com.haier.po.Tcase;
+import com.haier.po.TenvdetailCustom;
+import com.haier.po.Ti;
+import com.haier.po.Tservice;
+import com.haier.util.AssertUtil;
+import com.haier.util.HryHttpClientUtil;
 import org.testng.annotations.*;
 
 import java.lang.reflect.Method;
@@ -16,6 +21,9 @@ public class PgwTest {
     private Integer serviceId;
     private Integer envId;
     private String url;
+    private Tservice tservice;
+    private Ti ti;
+    private TenvdetailCustom tenvdetailCustom;
 
 
     @DataProvider(name="CaseDataProvider")
@@ -37,7 +45,9 @@ public class PgwTest {
     @Test(testName = "/tradeQueryFacade/tradeQuery",description = "交易查询")
     public void tradeQueryFacade_tradeQuery(List<Tcase> tcaseList){
         //测试用例
+        String actual = HryHttpClientUtil.send("", 1, 1, 1, "");
 
+        AssertUtil.supperAssert(1,"","",1);
     }
 
 
