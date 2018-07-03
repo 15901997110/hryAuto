11082f37d8349794ff8e8752e1e01b145d51b182
luqiwei <luqiwei@kjt.com.cn>
2018-07-03 13:40:28 +0800

    添加测试报告监听器,封装testNG运行方法

--- hryweb/src/main/java/com/haier/util/RunUtil.java
+++ hryweb/src/main/java/com/haier/util/RunUtil.java
@@ -5,9 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.testng.ITestNGListener;
 import org.testng.TestNG;
 import org.testng.internal.annotations.IListeners;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Description:
@@ -16,28 +22,66 @@ import java.util.List;
  */
 @Slf4j
 public class RunUtil {
-    public static void run(Integer userId,List<String> clazzs){
-        log.info("运行的测试集合:"+clazzs.toString());
+    public static void run(Integer userId, List<String> clazzs) {
+        log.info("运行的测试集合:" + clazzs.toString());
         List<String> distinctClazzs = HryUtil.distinct(clazzs);
-        if(distinctClazzs==null){
+        if (distinctClazzs == null) {
             return;
         }
 
-        List<Class> classList=new ArrayList<>();
-        for(String clazz:clazzs){
+        List<Class> classList = new ArrayList<>();
+        for (String clazz : clazzs) {
             try {
                 classList.add(Class.forName(clazz));
             } catch (ClassNotFoundException e) {
-                log.error("测试类"+clazz+"未找到",e);
+                log.error("测试类" + clazz + "未找到", e);
             }
         }
 
-        Class[] ccc=new Class[classList.size()];
+        Class[] ccc = new Class[classList.size()];
 
-        ITestNGListener listener=new HryReporter(userId);
-        TestNG testNG=new TestNG();
+        ITestNGListener listener = new HryReporter(userId);
+        TestNG testNG = new TestNG();
         testNG.addListener(listener);
         testNG.setTestClasses(classList.toArray(ccc));
         testNG.run();
+    }
+
+    private static void runNG(Map<String, String> params, List<String> classes) {
+        if (classes == null || classes.size() == 0) {
+            return;
+        }
+
+        String dateStr = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS").format(new Date());
+
+        //构建class
+        List<XmlClass> xmlClasses = new ArrayList<>();
+        for(String clazz:classes){
+            xmlClasses.add(new XmlClass(clazz));
+        }
+
+        //构建suite
+        XmlSuite suite = new XmlSuite();
+        suite.setName("AutoSuite_" + dateStr);
+        suite.setParameters(params);
+
+        //构建test
+        XmlTest test = new XmlTest(suite);
+        test.setName("AutuTest_" + dateStr);
+        test.setClasses(xmlClasses);
+
+
+        List<XmlSuite> xmlSuites=new ArrayList<>();
+        xmlSuites.add(suite);
+
+        List<Class<? extends ITestNGListener>> listeners=new ArrayList<>();
+        listeners.add(com.haier.testng.listener.ExtentTestNGIReporterListener.class);
+
+        TestNG ng=new TestNG();
+        ng.setXmlSuites(xmlSuites);
+        ng.setListenerClasses(listeners);
+
+        ng.run();
+
     }
 }
