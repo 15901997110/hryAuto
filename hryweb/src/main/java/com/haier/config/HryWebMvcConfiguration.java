72c24bc9ad5b380dcb84e1c903f8bc01b458e622
luqiwei <luqiwei@kjt.com.cn>
2018-07-03 17:42:45 +0800

    é¡访问项目外部页面

---
+++ hryweb/src/main/java/com/haier/config/HryWebMvcConfiguration.java
@@ -0,0 +1,20 @@
+package com.haier.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @Description: 访问项目外部的静态页面
+ * @Author: luqiwei
+ * @Date: 2018/7/3 17:20
+ */
+@Configuration
+public class HryWebMvcConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("../test-output/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+
+}
