571f2bec09e117f8827454d910d471df3ca21c16
luqiwei <luqiwei@kjt.com.cn>
2018-07-06 16:19:14 +0800

    dataprovider

---
+++ hryweb/src/main/java/com/haier/testng/provider/CaseProvider.java
@@ -0,0 +1,12 @@
+package com.haier.testng.provider;
+
+import org.testng.annotations.DataProvider;
+
+/**
+ * @Description: 根据测试方法,提供测试case数据
+ * @Author: luqiwei
+ * @Date: 2018/7/6 16:17
+ */
+public class CaseProvider {
+
+}
