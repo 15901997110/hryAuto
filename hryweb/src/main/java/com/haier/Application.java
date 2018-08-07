71062f9399d9be1889f77506a360446467a1f0c4
luqiwei <luqiwei@kjt.com.cn>
2018-08-07 17:54:14 +0800

    remove no used import

--- hryweb/src/main/java/com/haier/Application.java
+++ hryweb/src/main/java/com/haier/Application.java
@@ -3,12 +3,7 @@ package com.haier;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 @MapperScan("com.haier.mapper")
