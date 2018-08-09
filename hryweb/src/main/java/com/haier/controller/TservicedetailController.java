c72ef741a02ab5dd7e42f1cfacd3ba3e29310848
luqiwei <luqiwei@kjt.com.cn>
2018-08-09 18:09:14 +0800

    服务详情中校验逻辑优化

--- hryweb/src/main/java/com/haier/controller/TservicedetailController.java
+++ hryweb/src/main/java/com/haier/controller/TservicedetailController.java
@@ -1,15 +1,21 @@
 package com.haier.controller;
 
+import com.haier.enums.StatusCodeEnum;
+import com.haier.exception.HryException;
 import com.haier.po.Tservicedetail;
 import com.haier.po.TservicedetailCustom;
+import com.haier.po.TservicedetailExample;
 import com.haier.response.Result;
 import com.haier.service.TservicedetailService;
+import com.haier.util.ReflectUtil;
 import com.haier.util.ResultUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Description: 服务-环境映射关系
@@ -27,6 +33,20 @@ public class TservicedetailController {
     //增
     @PostMapping("/insertOne")
     public Result insertOne(Tservicedetail tservicedetail) {
+        ReflectUtil.setInvalidFieldToNull(tservicedetail, false);
+        if (tservicedetail == null || tservicedetail.getHostinfo() == null || tservicedetail.getServiceid() == null
+                || tservicedetail.getEnvid() == null) {
+            throw new HryException(StatusCodeEnum.PARAMETER_ERROR, "serviceId,envId,hostInfo必填");
+        }
+        //数据校验
+        Tservicedetail condition=new Tservicedetail();
+        condition.setServiceid(tservicedetail.getServiceid());
+        condition.setEnvid(tservicedetail.getEnvid());
+        List<Tservicedetail> tservicedetailList = this.selectList(condition);
+        if (tservicedetailList.size() > 0) {
+            throw new HryException(StatusCodeEnum.EXIST_RECORD, "tservicedetail表中serviceId=" + tservicedetail.getServiceid() + ",envId=" + tservicedetail.getEnvid() + "的数据已存在");
+        }
+        //插入数据
         return ResultUtil.success(tservicedetailService.insertOne(tservicedetail));
     }
 
@@ -39,6 +59,25 @@ public class TservicedetailController {
     //改
     @PostMapping("/updateOne")
     public Result updateOne(Tservicedetail tservicedetail) {
+        ReflectUtil.setInvalidFieldToNull(tservicedetail, false);
+        if (tservicedetail == null || tservicedetail.getId() == null) {
+            throw new HryException(StatusCodeEnum.PARAMETER_ERROR, "id必填");
+        }
+        //数据重复性校验
+        if (tservicedetail.getServiceid() != null && tservicedetail.getEnvid() != null) {
+            Tservicedetail condition=new Tservicedetail();
+            condition.setServiceid(tservicedetail.getServiceid());
+            condition.setEnvid(tservicedetail.getEnvid());
+            List<Tservicedetail> tservicedetailList = this.selectList(condition);
+            if (tservicedetailList.size() > 0) {
+                for (Tservicedetail t : tservicedetailList) {
+                    if (tservicedetail.getServiceid().equals(t.getServiceid()) && tservicedetail.getEnvid().equals(t.getEnvid()) && !tservicedetail.getId().equals(t.getId())) {
+                        throw new HryException(StatusCodeEnum.EXIST_RECORD, "请检查serviceKey和envKey");
+                    }
+                }
+            }
+        }
+
         return ResultUtil.success(tservicedetailService.updateOne(tservicedetail));
     }
 
@@ -56,12 +95,17 @@ public class TservicedetailController {
     }
 
 
-    //查-仅查询tservicedetail表数据,返回list
+    //查-仅查询tservicedetail表数据,返回list,但是使用Result来进行了包装
     @PostMapping("/selectTservicedetailListByCondition")
     public Result selectTservicedetailListByCondition(Tservicedetail tservicedetail) {
         return ResultUtil.success(tservicedetailService.selectByCondition(tservicedetail));
     }
 
+    //返回纯List
+    @PostMapping("/selectList")
+    public List<Tservicedetail> selectList(Tservicedetail tservicedetail){
+        return tservicedetailService.selectByCondition(tservicedetail);
+    }
 
     //查-关联表查询,返回包装类,并且携带pageinfo
     @PostMapping("/selectByCondition")
--- hryweb/src/main/java/com/haier/service/impl/TservicedetailServiceImpl.java
+++ hryweb/src/main/java/com/haier/service/impl/TservicedetailServiceImpl.java
@@ -36,24 +36,6 @@ public class TservicedetailServiceImpl implements TservicedetailService {
 
     @Override
     public Integer insertOne(Tservicedetail tservicedetail) {
-        if (tservicedetail == null || tservicedetail.getHostinfo() == null || tservicedetail.getServiceid() == null
-                || tservicedetail.getEnvid() == null
-                || "".equals(tservicedetail.getHostinfo().trim()) || tservicedetail.getServiceid() == 0 || tservicedetail.getEnvid() == 0) {
-            throw new HryException(StatusCodeEnum.PARAMETER_ERROR, "serviceId,envId,hostInfo必填");
-        }
-        ReflectUtil.setInvalidFieldToNull(tservicedetail, false);
-        //先查询数据是否有重复
-        TservicedetailExample tservicedetailExample = new TservicedetailExample();
-        tservicedetailExample.createCriteria()
-                .andServiceidEqualTo(tservicedetail.getServiceid())
-                .andEnvidEqualTo(tservicedetail.getEnvid())
-                .andStatusGreaterThan(0);
-
-        List<Tservicedetail> tservicedetailList = tservicedetailMapper.selectByExample(tservicedetailExample);
-        if (tservicedetailList != null && tservicedetailList.size() > 0) {
-            throw new HryException(StatusCodeEnum.EXIST_RECORD, "tservicedetail表中serviceId=" + tservicedetail.getServiceid() + ",envId=" + tservicedetail.getEnvid() + "的数据存在");
-        }
-        //插入数据
         return tservicedetailMapper.insertSelective(tservicedetail);
     }
 
@@ -117,18 +99,16 @@ public class TservicedetailServiceImpl implements TservicedetailService {
 
     @Override
     public Integer updateOne(Tservicedetail tservicedetail) {
-        ReflectUtil.setInvalidFieldToNull(tservicedetail, false);
-        if (tservicedetail == null || tservicedetail.getId() == null) {
-            throw new HryException(StatusCodeEnum.PARAMETER_ERROR, "id必填");
-        }
         return tservicedetailMapper.updateByPrimaryKeySelective(tservicedetail);
     }
 
     @Override
     public List<Tservicedetail> selectByCondition(Tservicedetail tservicedetail) {
         ReflectUtil.setFieldAddPercentAndCleanZero(tservicedetail, false);
-        TservicedetailExample tservicedetailExample = new TservicedetailExample();
-        TservicedetailExample.Criteria criteria = tservicedetailExample.createCriteria();
+        TservicedetailExample example = new TservicedetailExample();
+
+        TservicedetailExample.Criteria criteria = example.createCriteria();
+        criteria.andStatusGreaterThan(0);
         if (tservicedetail != null) {
             if (tservicedetail.getId() != null)
                 criteria.andIdEqualTo(tservicedetail.getId());
@@ -143,8 +123,8 @@ public class TservicedetailServiceImpl implements TservicedetailService {
             if (tservicedetail.getSwaggerurl() != null)
                 criteria.andSwaggerurlLike(tservicedetail.getSwaggerurl());
         }
-        criteria.andStatusGreaterThan(0);
-        List<Tservicedetail> tservicedetailList = tservicedetailMapper.selectByExample(tservicedetailExample);
+
+        List<Tservicedetail> tservicedetailList = tservicedetailMapper.selectByExample(example);
         return tservicedetailList;
     }
 
