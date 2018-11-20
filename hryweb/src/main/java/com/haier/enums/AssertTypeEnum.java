ad94c6535b0b9a0033536d51c0a7c1f4634ddf7e
luqiwei <luqiwei@kjt.com.cn>
2018-11-20 15:08:48 +0800

    断言类型新增exclude,断言不期望出现某些值,优化match与contaion的断言逻辑,contaion只断言包含关系,match只匹配正则表达式

--- hryweb/src/main/java/com/haier/enums/AssertTypeEnum.java
+++ hryweb/src/main/java/com/haier/enums/AssertTypeEnum.java
@@ -7,9 +7,18 @@ package com.haier.enums;
  */
 public enum AssertTypeEnum {
     EQUAL(1, "equal", "实际值与期望值完全相等"),//完全相等
+
     CONTAIN(2, "contain", "实际值包含期望值(忽略大小写)"),
+
     KEY_VALUE(3, "key-value", "实际值与相对应key的期望中的值相等,或忽略大小写比较相等,或忽略空字符比较相等,或包含期望值,或匹配期望值的正则形式,所有的key-value对比较都为true,断言通过"),
-    MATCH(4, "match", "实际值中匹配到期望值的正则表达式");
+
+    MATCH(4, "match", "实际值中匹配到期望值的正则表达式"),
+
+    EXCLUDE(5, "exclude", "实际值中不包括期望的值|正则表达式")
+
+
+    ;
+
 
     AssertTypeEnum(int id, String value, String desc) {
         this.id = id;
