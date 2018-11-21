cf57cd144f1c6e025885d963f6f2bebc9a34659b
luqiwei <luqiwei@kjt.com.cn>
2018-11-21 18:46:00 +0800

    自动生成测试类方法优化

--- hryweb/src/main/java/com/haier/service/AutocodeService.java
+++ hryweb/src/main/java/com/haier/service/AutocodeService.java
@@ -8,7 +8,9 @@ import java.util.Map;
  * @Date: 2018/7/25 11:20
  */
 public interface AutocodeService {
-    Map<String, String> generateBaseClass();
+/*    Map<String, String> generateBaseClass();
 
-    Map<String, String> generateDefaultTestClass();
+    Map<String, String> generateDefaultTestClass();*/
+
+    Map<String, String> generateTestClass();
 }
