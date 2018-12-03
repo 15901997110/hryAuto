788e43c4294c5a30b2e32d847e17f674f419534d
luqiwei <luqiwei@kjt.com.cn>
2018-12-03 18:50:08 +0800

    更新测试类

--- hryweb/src/main/java/com/haier/testng/test/CbpDefaultTest.java
+++ hryweb/src/main/java/com/haier/testng/test/CbpDefaultTest.java
@@ -15,6 +15,30 @@ public class CbpDefaultTest extends Base{
         super(serviceId, envId, caseDesigner, i_c, i_c_zdy, testingId);
     }
 
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.apply.facade.rs.ApplyResource/checkBdContactTel", dataProvider = "provider", description = "客户经理联系手机Check")
+    public void cbp_api_resource_com_cbp_biz_apply_facade_rs_ApplyResource_checkBdContactTel(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.apply.facade.rs.ApplyResource/checkYearAccountCode", dataProvider = "provider", description = "365账户查询")
+    public void cbp_api_resource_com_cbp_biz_apply_facade_rs_ApplyResource_checkYearAccountCode(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.apply.facade.rs.ApplyResource/createApply", dataProvider = "provider", description = "申请授信")
+    public void cbp_api_resource_com_cbp_biz_apply_facade_rs_ApplyResource_createApply(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.apply.facade.rs.ApplyResource/getApplyInfo", dataProvider = "provider", description = "申请信息取得")
+    public void cbp_api_resource_com_cbp_biz_apply_facade_rs_ApplyResource_getApplyInfo(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
     @Test(testName = "/cbp-api/resource/com.cbp.biz.cfLoan.facade.rs.CfLoanPrepayResource/createCfBuyBack", dataProvider = "provider", description = "提前回购申请")
     public void cbp_api_resource_com_cbp_biz_cfLoan_facade_rs_CfLoanPrepayResource_createCfBuyBack(HryTest test) {
         String actual = HryHttpClientUtil.send(test, this);
@@ -149,6 +173,126 @@ public class CbpDefaultTest extends Base{
 
     @Test(testName = "/cbp-api/resource/com.cbp.biz.cfLoan.facade.rs.CfLoanResource/queryCfXszrLoan", dataProvider = "provider", description = "2.1.2-P0010线上直融借款查询")
     public void cbp_api_resource_com_cbp_biz_cfLoan_facade_rs_CfLoanResource_queryCfXszrLoan(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.collection.facade.rs.DataCollectionResource/collection", dataProvider = "provider", description = "采集")
+    public void cbp_api_resource_com_cbp_biz_collection_facade_rs_DataCollectionResource_collection(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.collection.facade.rs.DataCollectionResource/createDataCollection", dataProvider = "provider", description = "数据采集")
+    public void cbp_api_resource_com_cbp_biz_collection_facade_rs_DataCollectionResource_createDataCollection(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.hryLoan.facade.rs.HryLoanResource/createHryLoan", dataProvider = "provider", description = "借款人融资申请")
+    public void cbp_api_resource_com_cbp_biz_hryLoan_facade_rs_HryLoanResource_createHryLoan(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.hryLoan.facade.rs.HryLoanResource/getHryBidLoan", dataProvider = "provider", description = "标的贷中信息")
+    public void cbp_api_resource_com_cbp_biz_hryLoan_facade_rs_HryLoanResource_getHryBidLoan(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.hryLoan.facade.rs.HryLoanResource/getHryFinancingContract", dataProvider = "provider", description = "获取借款人融资服务协议")
+    public void cbp_api_resource_com_cbp_biz_hryLoan_facade_rs_HryLoanResource_getHryFinancingContract(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.hryLoan.facade.rs.HryLoanResource/queryHryLoan", dataProvider = "provider", description = "借款人融资查询")
+    public void cbp_api_resource_com_cbp_biz_hryLoan_facade_rs_HryLoanResource_queryHryLoan(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.hryLoan.facade.rs.HryLoanResource/queryHryRepay", dataProvider = "provider", description = "借款人还款查询")
+    public void cbp_api_resource_com_cbp_biz_hryLoan_facade_rs_HryLoanResource_queryHryRepay(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.hryLoan.facade.rs.HryLoanResource/queryHryRepayPlan", dataProvider = "provider", description = "借款人还款计划查询")
+    public void cbp_api_resource_com_cbp_biz_hryLoan_facade_rs_HryLoanResource_queryHryRepayPlan(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.limit.facade.rs.LimitResource/queryLimit", dataProvider = "provider", description = "我的额度申请查询")
+    public void cbp_api_resource_com_cbp_biz_limit_facade_rs_LimitResource_queryLimit(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.limit.facade.rs.LimitResource/queryLimitAccount", dataProvider = "provider", description = "授信账户查询")
+    public void cbp_api_resource_com_cbp_biz_limit_facade_rs_LimitResource_queryLimitAccount(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.loan.facade.rs.LoanResource/createLoan", dataProvider = "provider", description = "借款申请")
+    public void cbp_api_resource_com_cbp_biz_loan_facade_rs_LoanResource_createLoan(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.loan.facade.rs.LoanResource/getAccount", dataProvider = "provider", description = "交易对方及工贸账户查询")
+    public void cbp_api_resource_com_cbp_biz_loan_facade_rs_LoanResource_getAccount(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.payment.facade.rs.PaymentDetailResource/queryPaymentDetail", dataProvider = "provider", description = "支付明细查询")
+    public void cbp_api_resource_com_cbp_biz_payment_facade_rs_PaymentDetailResource_queryPaymentDetail(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.prepay.facade.rs.PrepayResource/prepayApply", dataProvider = "provider", description = "提前还款申请")
+    public void cbp_api_resource_com_cbp_biz_prepay_facade_rs_PrepayResource_prepayApply(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.prepay.facade.rs.PrepayResource/prepayCalculate", dataProvider = "provider", description = "提前还款计算")
+    public void cbp_api_resource_com_cbp_biz_prepay_facade_rs_PrepayResource_prepayCalculate(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.prepay.facade.rs.PrepayResource/prepayList", dataProvider = "provider", description = "提前还款查询")
+    public void cbp_api_resource_com_cbp_biz_prepay_facade_rs_PrepayResource_prepayList(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.repay.facade.rs.RepayResource/createRepay", dataProvider = "provider", description = "放款")
+    public void cbp_api_resource_com_cbp_biz_repay_facade_rs_RepayResource_createRepay(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.repay.facade.rs.RepayResource/queryLoanRepay", dataProvider = "provider", description = "查询我的借款")
+    public void cbp_api_resource_com_cbp_biz_repay_facade_rs_RepayResource_queryLoanRepay(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.repay.facade.rs.RepayResource/queryRepaymentDetail", dataProvider = "provider", description = "查询还款明细")
+    public void cbp_api_resource_com_cbp_biz_repay_facade_rs_RepayResource_queryRepaymentDetail(HryTest test) {
+        String actual = HryHttpClientUtil.send(test, this);
+        supperAssert(actual, test);
+    }
+
+    @Test(testName = "/cbp-api/resource/com.cbp.biz.repay.facade.rs.RepayResource/repay", dataProvider = "provider", description = "还款")
+    public void cbp_api_resource_com_cbp_biz_repay_facade_rs_RepayResource_repay(HryTest test) {
         String actual = HryHttpClientUtil.send(test, this);
         supperAssert(actual, test);
     }
