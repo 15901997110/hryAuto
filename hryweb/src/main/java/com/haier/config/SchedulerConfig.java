96cdb634e4f7fb9b9aa1970af54bf6ada74b6164
luqiwei <luqiwei@kjt.com.cn>
2018-12-13 10:25:55 +0800

    quartz init

--- hryweb/src/main/java/com/haier/config/SchedulerConfig.java
+++ hryweb/src/main/java/com/haier/config/SchedulerConfig.java
@@ -1,6 +1,7 @@
 package com.haier.config;
 
 import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
@@ -13,7 +14,7 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 public class SchedulerConfig implements SchedulerFactoryBeanCustomizer {
     @Override
     public void customize(SchedulerFactoryBean schedulerFactoryBean) {
-        schedulerFactoryBean.setStartupDelay(2);
+        //schedulerFactoryBean.setStartupDelay(2);
         schedulerFactoryBean.setAutoStartup(true);
         schedulerFactoryBean.setOverwriteExistingJobs(true);
     }
--- hryweb/src/main/java/com/haier/job/RunCustomJob.java
+++ hryweb/src/main/java/com/haier/job/RunCustomJob.java
@@ -1,9 +1,23 @@
 package com.haier.job;
 
+import com.haier.config.SpringContextHolder;
+import com.haier.service.TcustomService;
+import org.quartz.*;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
 /**
  * @Description:
  * @Author: luqiwei
  * @Date: 2018/12/12 13:22
  */
-public class RunCustomJob {
+
+public class RunCustomJob extends QuartzJobBean {
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+        Integer customId = (Integer) jobDataMap.get("customId");
+        Integer executeUserId = (Integer) jobDataMap.get("executeUserId");
+        TcustomService tcustomService = SpringContextHolder.getBean(TcustomService.class);
+        tcustomService.run(customId, executeUserId);
+    }
 }
