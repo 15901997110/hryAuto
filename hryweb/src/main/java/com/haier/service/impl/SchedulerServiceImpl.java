e1007971d0c0ff23a507551f0088f51acfac094d
luqiwei <luqiwei@kjt.com.cn>
2018-12-19 09:50:46 +0800

    切换分支

--- hryweb/src/main/java/com/haier/service/impl/SchedulerServiceImpl.java
+++ hryweb/src/main/java/com/haier/service/impl/SchedulerServiceImpl.java
@@ -2,7 +2,8 @@ package com.haier.service.impl;
 
 import com.haier.job.RunCustomJob;
 import com.haier.service.SchedulerService;
-import org.quartz.JobDataMap;
+import org.quartz.*;
+import org.quartz.impl.triggers.CronTriggerImpl;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +16,19 @@ import org.springframework.stereotype.Service;
 public class SchedulerServiceImpl implements SchedulerService {
 
     public Boolean addJob(String jobName, String jobGroup,JobDataMap jobDataMap, String cronExp) {
+        if(!CronExpression.isValidExpression(cronExp)){
+
+        }
         JobDetailFactoryBean jobDetail=new JobDetailFactoryBean();
 
         jobDetail.setName(jobName);
         jobDetail.setGroup(jobGroup);
         jobDetail.setJobClass(RunCustomJob.class);
         jobDetail.setJobDataMap(jobDataMap);
+
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(cronExp).withMisfireHandlingInstructionDoNothing();
+        CronTrigger trigger= TriggerBuilder.newTrigger().withSchedule(scheduleBuilder).build();
+
 
 
 
