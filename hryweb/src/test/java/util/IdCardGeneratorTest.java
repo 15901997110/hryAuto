098df7e667737c852559d2961aa46f6ce36bf6c5
luqiwei <luqiwei@kjt.com.cn>
2018-12-29 18:10:27 +0800

    random id card

---
+++ hryweb/src/test/java/util/IdCardGeneratorTest.java
@@ -0,0 +1,18 @@
+package util;
+
+import com.haier.util.IdCardGenerator;
+import org.testng.annotations.Test;
+
+/**
+ * @Description:
+ * @Author: luqiwei
+ * @Date: 2018/12/29 17:53
+ */
+public class IdCardGeneratorTest {
+/*    @Test
+    public void generateTest() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(IdCardGenerator.generate());
+        }
+    }*/
+}
